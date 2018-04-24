de53646061e10b118a3e090c4c6da2073b0340e8
rmanguinho <rodrigo.manguinho@gmail.com>
2018-04-24 19:19:06 -0300

    Tela de cadastro

--- app/src/main/java/com/forwork/app/activities/SignInActivity.java
+++ app/src/main/java/com/forwork/app/activities/SignInActivity.java
@@ -1,14 +1,41 @@
 package com.forwork.app.activities;
 
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
+import android.view.View;
 
 import com.forwork.app.R;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class SignInActivity extends AppCompatActivity {
+    @BindView(R.id.til_email) TextInputLayout emailTextInputLayout;
+    @BindView(R.id.tie_email) TextInputEditText emailTextInputEditText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_in);
+    }
+
+    private boolean validate() {
+        if (TextUtils.isEmpty(emailTextInputEditText.getText())) {
+            emailTextInputLayout.setError("teste");
+            return false;
+        }
+        return true;
+    }
+
+    @OnClick(R.id.ivw_enter)
+    public void onEnterClick(View view) {
+        if (validate()) {
+            Intent intent = new Intent(this, SignUpActivity.class);
+            startActivity(intent);
+        }
     }
 }
